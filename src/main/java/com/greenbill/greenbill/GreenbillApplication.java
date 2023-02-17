1303a750c6d226a87427368fb1b34cb905c02dec
LiyanageSlik <150815B@uom.lk>
2023-02-17 08:35:40 +0530

    initial commit

---
+++ src/main/java/com/greenbill/greenbill/GreenbillApplication.java
@@ -0,0 +1,13 @@
+package com.greenbill.greenbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenbillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenbill/greenbill/GreenbillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenbill.greenbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
