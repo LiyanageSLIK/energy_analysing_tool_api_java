ba42c86ffd7efb415d81c35871d8190bcb0146ad
LiyanageSlik <150815B@uom.lk>
2023-02-18 13:11:52 +0530

    created SubscriptionEntity

---
+++ src/main/java/com/greenbill/greenbill/entity/SubscriptionEntity.java
@@ -0,0 +1,39 @@
+package com.greenbill.greenbill.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "subscription")
+public class SubscriptionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false, unique = true)
+    private LocalDate endDate;
+
+    @Column(name = "status", nullable = false)
+    private boolean status;
+
+    @ManyToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private UserEntity user;
+
+    @ManyToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private SubscriptionPlanEntity subscriptionPlan;
+
+
+}
