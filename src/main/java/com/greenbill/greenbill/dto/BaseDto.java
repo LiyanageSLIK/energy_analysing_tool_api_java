62577cd1d416aaa29fc80513da155792df312d31
LiyanageSlik <150815B@uom.lk>
2023-02-21 20:09:16 +0530

    BaseDto created

---
+++ src/main/java/com/greenbill/greenbill/dto/BaseDto.java
@@ -0,0 +1,10 @@
+package com.greenbill.greenbill.dto;
+
+import lombok.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseDto implements Serializable {
+}
