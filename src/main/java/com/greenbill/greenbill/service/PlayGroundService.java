2335395b1c5db0ab6c3c1625d2b30927ce1d92a9
LiyanageSlik <150815B@uom.lk>
2023-02-28 19:25:43 +0530

    service layer

---
+++ src/main/java/com/greenbill/greenbill/service/PlayGroundService.java
@@ -0,0 +1,54 @@
+package com.greenbill.greenbill.service;
+
+import com.greenbill.greenbill.dto.AddProjectReqResDto;
+import com.greenbill.greenbill.dto.AddSectionDto;
+import com.greenbill.greenbill.entity.ProjectEntity;
+import com.greenbill.greenbill.entity.SectionEntity;
+import com.greenbill.greenbill.repository.ApplianceRepository;
+import com.greenbill.greenbill.repository.ProjectRepository;
+import com.greenbill.greenbill.repository.SectionRepository;
+import com.greenbill.greenbill.repository.TokenRepository;
+import com.greenbill.greenbill.util.JwtUtil;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
+
+@Service
+public class PlayGroundService {
+    @Autowired
+    private SectionRepository sectionRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private ApplianceRepository applianceRepository;
+
+    @Transactional
+    public void addSection(AddSectionDto addSectionDto){
+        SectionEntity Ssection=new SectionEntity();
+        if(addSectionDto.getParentSectionId()==null){
+            SectionEntity section=new SectionEntity(addSectionDto);
+            Ssection=sectionRepository.save(section);
+        }else{
+            SectionEntity parentSection=sectionRepository.getFirstById(addSectionDto.getParentSectionId());
+            SectionEntity section=new SectionEntity(addSectionDto);
+            section.setParentSection(parentSection);
+            Ssection=sectionRepository.save(section);
+            for (SectionEntity sec:parentSection.getChildSections()
+            ) {
+                System.out.println(sec.getName());
+            }
+        }
+    }
+
+    public AddProjectReqResDto addProject(AddProjectReqResDto addProjectReqResDto)throws Exception {
+        ProjectEntity project=new ProjectEntity(addProjectReqResDto);
+        return new AddProjectReqResDto(projectRepository.save(project)) ;
+    }
+
+
+
+
+}
