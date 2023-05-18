8866f5be3d4f3624b35c00a4d55efc83bc94d552
Lahiru <lahirusampath.15@cse.mrt.ac.lk>
2023-05-18 09:20:20 +0530

    CORS issue

--- src/main/java/com/greenbill/greenbill/config/SecurityConfig.java
+++ src/main/java/com/greenbill/greenbill/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://3.111.156.196:80"));
+        configuration.setAllowedOrigins(List.of("*", "http://3.111.156.196:80", "http://3.111.156.196"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
