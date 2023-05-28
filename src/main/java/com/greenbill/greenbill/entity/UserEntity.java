da7ccc1a6f19fce9417087fb97bd072baf433df4
LiyanageSlik <150815B@uom.lk>
2023-05-28 11:40:56 +0530

    bug fix Auth

--- src/main/java/com/greenbill/greenbill/entity/UserEntity.java
+++ src/main/java/com/greenbill/greenbill/entity/UserEntity.java
@@ -73,7 +73,7 @@ public class UserEntity implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return List.of(new SimpleGrantedAuthority(role.toString()));
     }
 
     @Override
