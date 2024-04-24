28f4ce99915013b90b4583650fa2c4954bc6cee5
Lahiru <lahirusampath.15@cse.mrt.ac.lk>
2024-04-24 22:34:11 +0530

    CORS issue

--- src/main/java/com/greenbill/greenbill/config/ApplicationConfig.java
+++ src/main/java/com/greenbill/greenbill/config/ApplicationConfig.java
@@ -20,23 +20,4 @@ public class ApplicationConfig {
         authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
         return authProvider;
     }
-
-//    @Bean
-//    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
-//        DataSourceInitializer initializer = new DataSourceInitializer();
-//        initializer.setDataSource(dataSource);
-//        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
-//        initializer.setEnabled(true);
-//        return initializer;
-//    }
-
-//    @Bean
-//    public DataSource dataSource() {
-//        return DataSourceBuilder.create()
-//                .driverClassName("com.mysql.cj.jdbc.Driver")
-//                .url("jdbc:mysql://localhost:3306/greenbill?createDatabaseIfNotExist=true&useSSL=true")
-//                .username("root")
-//                .password("password")
-//                .build();
-//    }
 }
